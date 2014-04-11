b5989a0d027443a759bf7102248e2751cf4ef5e2
pyankoff <pyankoff@gmail.com>
2014-04-11 08:33:43 +0000

    updated web.js

--- web.js
+++ web.js
@@ -2,17 +2,16 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-var buf = new Buffer(256)
+var buf = new Buffer(256);
 buf = fs.readFileSync('index.html');
 
-var msg = buf.toString()
+var msg = buf.toString();
 
 app.get('/', function(request, response) {
-  response.send(msg);
+  response.send("fuck you");
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
