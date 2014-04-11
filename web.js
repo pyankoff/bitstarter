ba72caacc7ff1fd9cda98dcd4ccadbfd9d50525a
pyankoff <pyankoff@gmail.com>
2014-04-11 10:28:53 +0000

    new web.js

--- web.js
+++ web.js
@@ -3,12 +3,12 @@ var app = express();
 app.use(express.logger());
 
 
+var buf = new Buffer(256);
+buf = fs.readFileSync(path.join(process.cwd(), 'index.html'));
+var msg = buf.toString();
+
+
 app.get('/', function(request, response) {
-
-  var buf = new Buffer(256);
-  buf = fs.readFileSync('./index.html');
-
-  var msg = buf.toString();
   response.send('fuck you');
 });
 
